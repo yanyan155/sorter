f24fc2a500b90a6c77a8343f08843a68e0994633
yanyan155 <jm@tut.by>
2018-02-21 02:10:01 +0300

    100% tests

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+    this.compareFunction = function(a, b) { return a - b};
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
+    return this.arr;
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
   }
 
   sort(indices) {
     // your implementation
+    var indices = indices.sort(function(a, b) { return a - b});
+    var filtered = [];
+    for (var i = 0; i< indices.length; i++) {
+      filtered.push(this.arr[indices[i]]);
+    }
+    var res = filtered.sort(this.compareFunction);
+    for (var i = 0; i< indices.length; i++) {
+      this.arr[indices[i]] = filtered[i];
+    }
+    return this.arr;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    if (compareFunction){
+      this.compareFunction = compareFunction;
+      return compareFunction;
+    }
+    var res = function(a, b) { return a - b};
+    return res;
   }
 }
-
 module.exports = Sorter;
